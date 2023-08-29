bca15d0044b4f16cab54d364b8c41637b0efd88f
Régis Longchamp <97044425+regislon@users.noreply.github.com>
2023-08-29 09:13:43 +0200

    feat() : project setup

---
+++ tests/test_hello.py
@@ -0,0 +1,6 @@
+from zarr_geospatial_playground.foo import bar
+
+
+def test_bar():
+    # Test the return type
+    assert isinstance(bar(), str)
---
+++ zarr_geospatial_playground/foo.py
@@ -0,0 +1,31 @@
+import sys
+
+import appeal
+
+app = appeal.Appeal()
+
+
+def bar() -> str:
+    """Returns the Python version used to run this script.
+
+    Returns
+    -------
+    str
+        Python version
+    """
+    return sys.version
+
+
+@app.command()
+def hello(name: str) -> None:
+    """Prints a greeting to the console.
+
+    Parameters
+    ----------
+    name : str
+        Name of the person to greet
+    """
+    print(f"Hello, {name}!")
+
+
+app.main()
